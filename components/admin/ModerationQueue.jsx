5526ad913d1b60dbc48b506a0e4bfc4b28a5f03d
John Dudmesh <john@johndudmesh.com>
2021-07-05 11:30:26 +0200

    Added feedback for mod queue

--- components/admin/ModerationQueue.jsx
+++ components/admin/ModerationQueue.jsx
@@ -16,37 +16,55 @@ import React, { useEffect, useState } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 
-import { selectModerationQueue } from "../../redux/adminSlice";
+import { selectModerationQueue, selectAdminActionState } from "../../redux/adminSlice";
 import { fetchModerationQueue } from "../../redux/adminActions";
 
-import { Paper, Button } from "@material-ui/core";
+import { Paper, Typography } from "@material-ui/core";
+import PulseLoader from "react-spinners/PulseLoader";
 
 import { Post } from "../Post";
 
 import styles from "../../styles/Admin.module.scss";
+
+import { LoadingState } from "../../redux/constants";
 
 export function ModerationQueue({}) {
 
     const dispatch = useDispatch();
 
     const moderationQueue = useSelector(selectModerationQueue);
+    const loadingState = useSelector(selectAdminActionState);
 
     useEffect(() => {
         dispatch(fetchModerationQueue());
     }, []);
 
     const renderPosts = () => {
-        return moderationQueue.map((post, ix) => {
-            return <Paper key={ix} className={styles.post}>
-                <Post post={post} />
-            </Paper>
-        })
+        if(moderationQueue.length > 0) {
+            return moderationQueue.map((post, ix) => {
+                return <Paper key={ix} className={styles.post}>
+                    <Post post={post} />
+                </Paper>
+            })
+        } else {
+            return <Typography variant="h6">The moderation queue is empty</Typography>
+        }
     }
+
+    const renderLoadingSpinner = () => {
+        return <div className="loadingSpinner">
+            <PulseLoader color="#2A5880"></PulseLoader>
+        </div>
+    }
+
 
     return <div className={styles.postContainer}>
         <div>
             <div className="discussionList">
-                { renderPosts() }
+                { loadingState == LoadingState.Loaded
+                    ? renderPosts()
+                    : renderLoadingSpinner()
+                }
             </div>
         </div>
     </div>
